efe781575e9fe031c4bf16e07a539da442205853
realpoap <alapetite.mathieu@gmail.com>
2025-07-11 10:42:29 -0400

    exe 12_21: production containers

--- visual-regtest-docker/front/vite.config.ts
+++ visual-regtest-docker/front/vite.config.ts
@@ -1,38 +1,28 @@
-import tailwindcss from "@tailwindcss/vite"
-import react from '@vitejs/plugin-react'
-import path from "path"
-import { defineConfig } from 'vite'
-import eslint from 'vite-plugin-eslint'
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    eslint({
-      include: ['src/**/*.ts', 'src/**/*.tsx'],
-      exclude: ['node_modules/**', 'dist/**'],
-      cache: true,
-      fix: true
-    })
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  optimizeDeps: {
-    include: ['puppeteer', 'puppeteer-core'],
-    esbuildOptions: {
-      target: 'esnext'
-    }
-  },
-  build: {
-    commonjsOptions: {
-      include: [/node_modules/],
-    },
-    target: 'esnext'
-  },
-  server:{
-    allowedHosts:["regtest-front-dev"],
-  }
-}) 
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	optimizeDeps: {
+		include: ['puppeteer', 'puppeteer-core'],
+		esbuildOptions: {
+			target: 'esnext',
+		},
+	},
+	build: {
+		commonjsOptions: {
+			include: [/node_modules/],
+		},
+		target: 'esnext',
+	},
+	server: {
+		allowedHosts: ['regtest-front-dev'],
+	},
+});
